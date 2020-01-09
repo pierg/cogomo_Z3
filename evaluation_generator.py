511beaa5d10b59b1faa6e61dbe4c4211c610d89c
pierg <nmall843r>
2020-01-09 15:04:31 +0100

    added better prints

--- evaluation_generator.py
+++ evaluation_generator.py
@@ -100,10 +100,12 @@ def gen_file(n_props, n_comps):
 
         f.write("    times = {}\n")
 
-        for g in list_of_guarantees:
+        for i, g in enumerate(list_of_guarantees):
             f.write("    print('')\n".format(n_prop, n_comp))
             f.write("    print('')\n".format(n_prop, n_comp))
-            f.write("    print('Starting evaluation for {0} propositions and {1} components looking for proposition {2}')\n".format(n_prop, n_comp, g))
+            f.write("    print('Starting evaluation for {0} propositions and {1} components "
+                    "looking for proposition {2}/{3} (p{4})')"
+                    "\n".format(n_prop, n_comp, i+1, len(list_of_guarantees)+1, g))
             f.write("    start_time = time.time()\n")
             f.write("    n = len(components_selection(component_library, "
                     "Contract(assumptions=[], guarantees=[p{0} == True])))\n".format(str(g)))
